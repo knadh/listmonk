043a3f37a1bc68f949814babc13e946de9957b75
lovejieer <64838311+lovejieer@users.noreply.github.com>
2020-08-18 03:16:05 -0400

    Update ListForm.vue

--- frontend/src/views/ListForm.vue
+++ frontend/src/views/ListForm.vue
@@ -1,124 +1,197 @@
 <template>
-  <form @submit.prevent="onSubmit">
-    <div class="modal-card content" style="width: auto">
-      <header class="modal-card-head">
-        <p v-if="isEditing" class="has-text-grey-light is-size-7">
-          ID: {{ data.id }} / UUID: {{ data.uuid }}
-        </p>
-        <b-tag v-if="isEditing" :class="[data.type, 'is-pulled-right']">{{ data.type }}</b-tag>
-        <h4 v-if="isEditing">{{ data.name }}</h4>
-        <h4 v-else>New list</h4>
+  <section class="lists">
+    <header class="columns">
+      <div class="column is-two-thirds">
+        <h1 class="title is-4">Lists
+          <span v-if="!isNaN(lists.total)">({{ lists.total }})</span>
+        </h1>
+      </div>
+      <div class="column has-text-right">
+        <b-button type="is-primary" icon-left="plus" @click="showNewForm">New</b-button>
+      </div>
+    </header>
 
-      </header>
-      <section expanded class="modal-card-body">
-        <b-field label="Name" label-position="on-border">
-          <b-input :maxlength="200" :ref="'focus'" v-model="form.name"
-            placeholder="Name" required></b-input>
-        </b-field>
+    <b-table
+      :data="lists.results"
+      :loading="loading.lists"
+      :row-class="highlightedRow"
+      paginated backend-pagination pagination-position="both" @page-change="onPageChange"
+      :current-page="queryParams.page" :per-page="lists.perPage" :total="lists.total"
+      hoverable
+      default-sort="createdAt">
+        <template slot-scope="props">
+            <b-table-column field="name" label="Name" sortable width="25%">
+              <div>
+                <router-link :to="{name: 'subscribers_list', params: { listID: props.row.id }}">
+                  {{ props.row.name }}
+                </router-link>
+                <b-taglist>
+                    <b-tag class="is-small" v-for="t in props.row.tags" :key="t">{{ t }}</b-tag>
+                </b-taglist>
+              </div>
+            </b-table-column>
 
-        <b-field label="Type" label-position="on-border"
-          message="Public lists are open to the world to subscribe
-                   and their names may appear on public pages such as the subscription
-                   management page.">
-          <b-select v-model="form.type" placeholder="Type" required>
-            <option value="private">Private</option>
-            <option value="public">Public</option>
-          </b-select>
-        </b-field>
+            <b-table-column field="type" label="Type" sortable>
+              <div>
+                <b-tag :class="props.row.type">{{ props.row.type }}</b-tag>
+                {{ ' ' }}
+                <b-tag>
+                  <b-icon :icon="props.row.optin === 'double' ?
+                    'account-check-outline' : 'account-off-outline'" size="is-small" />
+                  {{ ' ' }}
+                  {{ props.row.optin }}
+                </b-tag>{{ ' ' }}
+                <a v-if="props.row.optin === 'double'" class="is-size-7 send-optin"
+                  href="#" @click="$utils.confirm(null, () => createOptinCampaign(props.row))">
+                  <b-tooltip label="Send opt-in campaign" type="is-dark">
+                    <b-icon icon="rocket-launch-outline" size="is-small" />
+                    Send opt-in campaign
+                  </b-tooltip>
+                </a>
+              </div>
+            </b-table-column>
 
-        <b-field label="Opt-in" label-position="on-border"
-          message="Double opt-in sends an e-mail to the subscriber asking for
-                   confirmation. On Double opt-in lists, campaigns are only sent to
-                   confirmed subscribers.">
-          <b-select v-model="form.optin" placeholder="Opt-in type" required>
-            <option value="single">Single</option>
-            <option value="double">Double</option>
-          </b-select>
-        </b-field>
+            <b-table-column field="subscriberCount" label="Subscribers" numeric sortable centered>
+                <router-link :to="`/subscribers/lists/${props.row.id}`">
+                  {{ props.row.subscriberCount }}
+                </router-link>
+            </b-table-column>
 
-        <b-field label="Tags" label-position="on-border">
-          <b-taginput v-model="form.tags" ellipsis
-            icon="tag-outline" placeholder="Tags"></b-taginput>
-        </b-field>
-      </section>
-      <footer class="modal-card-foot has-text-right">
-        <b-button @click="$parent.close()">Close</b-button>
-        <b-button native-type="submit" type="is-primary"
-          :loading="loading.lists">Save</b-button>
-      </footer>
-    </div>
-  </form>
+            <b-table-column field="createdAt" label="Created" sortable>
+                {{ $utils.niceDate(props.row.createdAt) }}
+            </b-table-column>
+            <b-table-column field="updatedAt" label="Updated" sortable>
+                {{ $utils.niceDate(props.row.updatedAt) }}
+            </b-table-column>
+
+            <b-table-column class="actions" align="right">
+              <div>
+                <router-link :to="`/campaign/new?list_id=${props.row.id}`">
+                  <b-tooltip label="Send campaign" type="is-dark">
+                    <b-icon icon="rocket-launch-outline" size="is-small" />
+                  </b-tooltip>
+                </router-link>
+                <a href="" @click.prevent="showEditForm(props.row)">
+                  <b-tooltip label="Edit" type="is-dark">
+                    <b-icon icon="pencil-outline" size="is-small" />
+                  </b-tooltip>
+                </a>
+                <a href="" @click.prevent="deleteList(props.row)">
+                  <b-tooltip label="Delete" type="is-dark">
+                    <b-icon icon="trash-can-outline" size="is-small" />
+                  </b-tooltip>
+                </a>
+              </div>
+            </b-table-column>
+        </template>
+
+        <template slot="empty" v-if="!loading.lists">
+            <empty-placeholder />
+        </template>
+    </b-table>
+
+    <!-- Add / edit form modal -->
+    <b-modal scroll="keep" :aria-modal="true" :active.sync="isFormVisible" :width="600">
+      <list-form :data="curItem" :isEditing="isEditing" @finished="formFinished"></list-form>
+    </b-modal>
+  </section>
 </template>
 
 <script>
 import Vue from 'vue';
 import { mapState } from 'vuex';
+import ListForm from './ListForm.vue';
+import EmptyPlaceholder from '../components/EmptyPlaceholder.vue';
 
 export default Vue.extend({
-  name: 'ListForm',
-
-  props: {
-    data: {},
-    isEditing: null,
+  components: {
+    ListForm,
+    EmptyPlaceholder,
   },
 
   data() {
     return {
-      // Binds form input values.
-      form: {
-        name: '',
-        type: '',
-        optin: '',
-        tags: '',
+      // Current list item being edited.
+      curItem: null,
+      isEditing: false,
+      isFormVisible: false,
+      queryParams: {
+        page: 1,
       },
     };
   },
 
   methods: {
-    onSubmit() {
-      if (this.isEditing) {
-        this.updateList();
-        return;
-      }
-
-      this.createList();
+    // Show the edit list form.
+    showEditForm(list) {
+      this.curItem = list;
+      this.isFormVisible = true;
+      this.isEditing = true;
     },
 
-    createList() {
-      this.$api.createList(this.form).then((data) => {
-        this.$emit('finished');
-        this.$parent.close();
-        this.$buefy.toast.open({
-          message: `'${data.name}' created`,
-          type: 'is-success',
-          queue: false,
-        });
+    // Show the new list form.
+    showNewForm() {
+      this.curItem = {};
+      this.isFormVisible = true;
+      this.isEditing = false;
+    },
+
+    formFinished() {
+      this.getLists();
+    },
+
+    deleteList(list) {
+      this.$utils.confirm(
+        'Are you sure? This does not delete subscribers.',
+        () => {
+          this.$api.deleteList(list.id).then(() => {
+            this.getLists();
+
+            this.$buefy.toast.open({
+              message: `'${list.name}' deleted`,
+              type: 'is-success',
+              queue: false,
+            });
+          });
+        },
+      );
+    },
+
+    onPageChange(p) {
+      this.queryParams.page = p;
+      this.getLists();
+    },
+
+    getLists() {
+      this.$api.getLists({
+        page: this.queryParams.page,
       });
     },
 
-    updateList() {
-      this.$api.updateList({ id: this.data.id, ...this.form }).then((data) => {
-        this.$emit('finished');
-        this.$parent.close();
-        this.$buefy.toast.open({
-          message: `'${data.name}' updated`,
-          type: 'is-success',
-          queue: false,
-        });
+    createOptinCampaign(list) {
+      const data = {
+        name: `Opt-in to ${list.name}`,
+        subject: `Confirm subscription(s) ${list.name}`,
+        lists: [list.id],
+        from_email: this.serverConfig.fromEmail,
+        content_type: 'richtext',
+        messenger: 'email',
+        type: 'optin',
+      };
+
+      this.$api.createCampaign(data).then((d) => {
+        this.$router.push({ name: 'campaign', hash: '#content', params: { id: d.id } });
       });
+      return false;
     },
   },
 
   computed: {
-    ...mapState(['loading']),
+    ...mapState(['lists', 'serverConfig', 'loading']),
   },
 
   mounted() {
-    this.form = { ...this.$props.data };
-
-    this.$nextTick(() => {
-      this.$refs.focus.focus();
-    });
+    this.getLists();
   },
 });
 </script>
