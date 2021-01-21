869a55c1efcbef5ce7f63cb60273d5c8275d2588
Karan Sharma <hello@mrkaran.dev>
2021-01-21 21:36:32 +0530

    feat: Add shell script for demo setup

---
+++ install-demo.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+# Listmonk demo setup using `docker-compose`.
+#
+# See https://listmonk.app/docs/installation/ for detailed installation steps.
+#
+
+
+check_dependency() {
+	if ! command -v curl > /dev/null; then
+		echo "curl is not installed."
+		exit 1
+    fi
+
+	if ! command -v docker > /dev/null; then
+		echo "docker is not installed."
+		exit 1
+    fi
+
+	if ! command -v docker-compose > /dev/null; then
+		echo "docker-compose is not installed."
+		exit 1
+	fi
+}
+
+setup_containers() {
+    curl -o docker-compose.yml https://raw.githubusercontent.com/knadh/listmonk/master/docker-compose.yml
+    docker-compose up -d demo-db demo-app
+}
+
+show_output(){
+    echo -e "\nListmonk is now up and running. Visit http://localhost:9000 in your browser.\n"
+}
+
+
+check_dependency
+setup_containers
+show_output
