df46e4e21578fc4c104dc22f49526d584d33a6cb
SweetPPro <sweetppro@users.noreply.github.com>
2021-11-08 21:19:18 +0100

    improved mobile navbar/sidebar
    Sidebar is hidden and all menu items moved to hamburger menu on mobile devices

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,9 +10,7 @@
           </div>
         </template>
         <template slot="end">
-            <b-navbar-item tag="div">
-              <a href="#" @click.prevent="doLogout">{{ $t('users.logout') }}</a>
-            </b-navbar-item>
+            <navigation :activeItem="activeItem" :activeGroup="activeGroup" :showLogout="true" :isSideBar="false" :isMobile="isMobile"/>
         </template>
     </b-navbar>
 
@@ -20,88 +18,14 @@
       <section class="sidebar">
         <b-sidebar
           position="static"
-          mobile="reduce"
+          mobile="hide"
           :fullheight="true"
           :open="true"
           :can-cancel="false"
         >
           <div>
             <b-menu :accordion="false">
-              <b-menu-list>
-                <b-menu-item :to="{name: 'dashboard'}" tag="router-link"
-                  :active="activeItem.dashboard"
-                  icon="view-dashboard-variant-outline" :label="$t('menu.dashboard')">
-                </b-menu-item><!-- dashboard -->
-
-                <b-menu-item :expanded="activeGroup.lists"
-                  :active="activeGroup.lists" data-cy="lists"
-                  v-on:update:active="(state) => toggleGroup('lists', state)"
-                  icon="format-list-bulleted-square" :label="$t('globals.terms.lists')">
-                  <b-menu-item :to="{name: 'lists'}" tag="router-link"
-                    :active="activeItem.lists" data-cy="all-lists"
-                    icon="format-list-bulleted-square" :label="$t('menu.allLists')"></b-menu-item>
-
-                  <b-menu-item :to="{name: 'forms'}" tag="router-link"
-                    :active="activeItem.forms" class="forms"
-                    icon="newspaper-variant-outline" :label="$t('menu.forms')"></b-menu-item>
-                </b-menu-item><!-- lists -->
-
-                <b-menu-item :expanded="activeGroup.subscribers"
-                  :active="activeGroup.subscribers" data-cy="subscribers"
-                  v-on:update:active="(state) => toggleGroup('subscribers', state)"
-                  icon="account-multiple" :label="$t('globals.terms.subscribers')">
-                  <b-menu-item :to="{name: 'subscribers'}" tag="router-link"
-                    :active="activeItem.subscribers" data-cy="all-subscribers"
-                    icon="account-multiple" :label="$t('menu.allSubscribers')"></b-menu-item>
-
-                  <b-menu-item :to="{name: 'import'}" tag="router-link"
-                    :active="activeItem.import" data-cy="import"
-                    icon="file-upload-outline" :label="$t('menu.import')"></b-menu-item>
-
-                  <b-menu-item :to="{name: 'bounces'}" tag="router-link"
-                    :active="activeItem.bounces" data-cy="bounces"
-                    icon="email-bounce" :label="$t('globals.terms.bounces')"></b-menu-item>
-                </b-menu-item><!-- subscribers -->
-
-                <b-menu-item :expanded="activeGroup.campaigns"
-                  :active="activeGroup.campaigns" data-cy="campaigns"
-                  v-on:update:active="(state) => toggleGroup('campaigns', state)"
-                  icon="rocket-launch-outline" :label="$t('globals.terms.campaigns')">
-                  <b-menu-item :to="{name: 'campaigns'}" tag="router-link"
-                    :active="activeItem.campaigns" data-cy="all-campaigns"
-                    icon="rocket-launch-outline" :label="$t('menu.allCampaigns')"></b-menu-item>
-
-                  <b-menu-item :to="{name: 'campaign', params: {id: 'new'}}" tag="router-link"
-                    :active="activeItem.campaign" data-cy="new-campaign"
-                    icon="plus" :label="$t('menu.newCampaign')"></b-menu-item>
-
-                  <b-menu-item :to="{name: 'media'}" tag="router-link"
-                    :active="activeItem.media" data-cy="media"
-                    icon="image-outline" :label="$t('menu.media')"></b-menu-item>
-
-                  <b-menu-item :to="{name: 'templates'}" tag="router-link"
-                    :active="activeItem.templates" data-cy="templates"
-                    icon="file-image-outline" :label="$t('globals.terms.templates')"></b-menu-item>
-
-                  <b-menu-item :to="{name: 'campaignAnalytics'}" tag="router-link"
-                    :active="activeItem.campaignAnalytics" data-cy="analytics"
-                    icon="chart-bar" :label="$t('globals.terms.analytics')"></b-menu-item>
-                </b-menu-item><!-- campaigns -->
-
-                <b-menu-item :expanded="activeGroup.settings"
-                  :active="activeGroup.settings" data-cy="settings"
-                  v-on:update:active="(state) => toggleGroup('settings', state)"
-                  icon="cog-outline" :label="$t('menu.settings')">
-
-                  <b-menu-item :to="{name: 'settings'}" tag="router-link"
-                    :active="activeItem.settings" data-cy="all-settings"
-                    icon="cog-outline" :label="$t('menu.settings')"></b-menu-item>
-
-                  <b-menu-item :to="{name: 'logs'}" tag="router-link"
-                    :active="activeItem.logs" data-cy="logs"
-                    icon="newspaper-variant-outline" :label="$t('menu.logs')"></b-menu-item>
-                </b-menu-item><!-- settings -->
-              </b-menu-list>
+              <navigation :activeItem="activeItem" :activeGroup="activeGroup" :showLogout="false" :isSideBar="true" />
             </b-menu>
           </div>
         </b-sidebar>
@@ -138,13 +62,22 @@ import Vue from 'vue';
 import { mapState } from 'vuex';
 import { uris } from './constants';
 
+import Navigation from './components/Navigation.vue';
+
 export default Vue.extend({
   name: 'App',
+
+  components: {
+    Navigation,
+  },
 
   data() {
     return {
       activeItem: {},
       activeGroup: {},
+      showLogout: Boolean,
+      isSideBar: Boolean,
+      windowWidth: window.innerWidth,
     };
   },
 
@@ -204,12 +137,20 @@ export default Vue.extend({
     version() {
       return process.env.VUE_APP_VERSION;
     },
+
+    isMobile() {
+      return this.windowWidth <= 768;
+    },
   },
 
   mounted() {
     // Lists is required across different views. On app load, fetch the lists
     // and have them in the store.
     this.$api.getLists({ minimal: true });
+
+    window.addEventListener('resize', () => {
+      this.windowWidth = window.innerWidth;
+    });
   },
 });
 </script>
