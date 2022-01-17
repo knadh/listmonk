893dff33f7351722e48fa6120f894f40eeb237ac
rbrn <costinaldea@yahoo.co.uk>
2022-01-17 15:13:38 +0000

    form params to url params

--- cmd/queries.go
+++ cmd/queries.go
@@ -36,6 +36,7 @@ type Queries struct {
 	// Non-prepared arbitrary subscriber queries.
 	QuerySubscribers                       string `query:"query-subscribers"`
 	QuerySubscribersByUserid               string `query:"query-subscribers-by-userid"`
+	QuerySubscribersByUserIdCount          string `query:"query-subscribers-count-by-userid"`
 	QuerySubscribersCount                  string `query:"query-subscribers-count"`
 	QuerySubscribersForExport              string `query:"query-subscribers-for-export"`
 	QuerySubscribersTpl                    string `query:"query-subscribers-template"`
