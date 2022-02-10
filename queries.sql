f48dee3e93bb539c2428ce6cc0ce6529dff00f46
shina charles memud <charlesmudy@gmail.com>
2022-02-10 02:28:52 +0100

    subscriber lists testing

--- queries.sql
+++ queries.sql
@@ -256,12 +256,9 @@ SELECT subscribers.* FROM subscribers
 -- with every resultant row.
 -- %s = arbitrary expression, %s = order by field, %s = order direction
 SELECT subscribers.*,  coalesce (lists.channel, 'NA') as channel FROM subscribers
-    LEFT JOIN subscriber_lists
-    ON (
-        -- Optional list filtering.
-        (CASE WHEN CARDINALITY($1::INT[]) > 0 THEN true ELSE false END)
-        AND subscriber_lists.subscriber_id = subscribers.id
-    )
+                              LEFT JOIN subscriber_lists
+                                        ON  subscriber_lists.subscriber_id = subscribers.id
+                              LEFT JOIN lists ON ( subscriber_lists.list_id = lists.id )
 WHERE subscribers.userid = $3 AND
    CASE WHEN coalesce( trim($4::varchar),'')='' THEN 1 = 1 ELSE lists.channel = $4 END
     %s
@@ -270,12 +267,9 @@ ORDER BY %s %s OFFSET $1 LIMIT (CASE WHEN $2 = 0 THEN NULL ELSE $2 END);
 -- name: query-subscribers-by-userid-count
 -- Replica of query-subscribers for obtaining the results count.
 SELECT COUNT(*) AS total FROM subscribers
-    LEFT JOIN subscriber_lists
-    ON (
-        -- Optional list filtering.
-        (CASE WHEN CARDINALITY($1::INT[]) > 0 THEN true ELSE false END)
-        AND subscriber_lists.subscriber_id = subscribers.id
-    )
+                                  LEFT JOIN subscriber_lists
+                                            ON  subscriber_lists.subscriber_id = subscribers.id
+                                  LEFT JOIN lists ON ( subscriber_lists.list_id = lists.id )
 WHERE subscribers.userid = $1 AND CASE WHEN coalesce( trim($2::varchar),'')='' THEN 1 = 1 ELSE lists.channel = $2 END  %s;
 
 -- name: query-subscribers-count-by-userid
