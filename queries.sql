12cc178b8ef28a02e4003c303b06e1f7959c6051
rbrn <costinaldea@yahoo.co.uk>
2022-02-11 16:28:15 +0000

    YOUR-18: save campaign correctly

--- queries.sql
+++ queries.sql
@@ -471,9 +471,12 @@ counts AS (
     AND subscribers.status='enabled'
 ),
 camp AS (
-    INSERT INTO campaigns (uuid, type, name, subject, from_email, body, altbody, content_type, send_at, headers, tags, messenger, template_id, to_send, max_subscriber_id)
-        SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, (SELECT id FROM tpl), (SELECT to_send FROM counts), (SELECT max_sub_id FROM counts)
-        RETURNING id
+    INSERT INTO campaigns (uuid, type, name, subject, from_email, body, altbody, content_type, send_at, headers, tags, messenger, template_id, to_send, max_subscriber_id, userid)
+        SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,
+               (SELECT id FROM tpl),
+               (SELECT to_send FROM counts), (SELECT max_sub_id FROM counts),
+               $15
+               RETURNING id
 )
 INSERT INTO campaign_lists (campaign_id, list_id, list_name)
     (SELECT (SELECT id FROM camp), id, name FROM lists WHERE id=ANY($14::INT[]))
