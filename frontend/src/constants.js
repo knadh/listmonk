70387bb1151d63b1bd1326774a417f34a64406d1
shina charles memud <charlesmudy@gmail.com>
2022-02-24 06:12:12 +0100

    Styling

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -36,7 +36,7 @@ export const storeKeys = Object.freeze({
 export const timestamp = 'ddd D MMM YYYY, hh:mm A';
 
 export const colors = Object.freeze({
-  primary: '#0055d4',
+  primary: '#6e24ff',
 });
 
 export const regDuration = '[0-9]+(ms|s|m|h|d)';
