f577522fe1730524653ffbd3bda7c2f6fafaa304
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-06-03 14:52:13 +0200

    Update inlang.config.js (#1359)

--- inlang.config.js
+++ inlang.config.js
@@ -1,34 +1,17 @@
-/**
- * See https://inlang.com/documentation
- */
 export async function defineConfig(env) {
-    const plugin = await env.$import(
-        "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
-    );
+	const { default: pluginJson } = await env.$import(
+		'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2.3.1/dist/index.js'
+	);
 
-    const pluginConfig = {
-        pathPattern: "./i18n/{language}.json",
-    };
+	const { default: standardLintRules } = await env.$import(
+		'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
+	);
 
-    return {
-        referenceLanguage: "en",
-        languages: await getLanguages(env),
-        readResources: (args) =>
-            plugin.readResources({ ...args, ...env, pluginConfig }),
-        writeResources: (args) =>
-            plugin.writeResources({ ...args, ...env, pluginConfig }),
-    };
-}
-
-
-/**
- * Automatically derives the languages in this repository.
- */
-async function getLanguages(env) {
-    // replace the path
-    const files = await env.$fs.readdir("./i18n");
-    // files that end with .json
-    // remove the .json extension to only get language name
-    const languages = files.filter((name) => name.endsWith(".json")).map((name) => name.replace(".json", ""));
-    return languages;
+	return {
+		referenceLanguage: 'en',
+		plugins: [pluginJson({ 
+			pathPattern: './i18n/{language}.json',
+			variableReferencePattern: ["{", "}"]
+		}), standardLintRules()]
+	};
 }
