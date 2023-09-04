daf5dcb99abf47a6ac043ff661530958577375e6
Aman Saini <amansaini0003@gmail.com>
2023-09-04 17:31:49 +0530

    refractor: handleGetLists function

--- cmd/lists.go
+++ cmd/lists.go
@@ -11,26 +11,17 @@ import (
 
 // handleGetLists retrieves lists with additional metadata like subscriber counts. This may be slow.
 func handleGetLists(c echo.Context) error {
-	var (
-		app = c.Get("app").(*App)
-		pg  = app.paginator.NewFromURL(c.Request().URL.Query())
+	app := c.Get("app").(*App)
+	pg := app.paginator.NewFromURL(c.Request().URL.Query())
 
-		query      = strings.TrimSpace(c.FormValue("query"))
-		orderBy    = c.FormValue("order_by")
-		order      = c.FormValue("order")
-		minimal, _ = strconv.ParseBool(c.FormValue("minimal"))
-		listID, _  = strconv.Atoi(c.Param("id"))
-
-		out models.PageResults
-	)
+	query := strings.TrimSpace(c.FormValue("query"))
+	orderBy := c.FormValue("order_by")
+	order := c.FormValue("order")
+	minimal, _ := strconv.ParseBool(c.FormValue("minimal"))
+	listID, _ := strconv.Atoi(c.Param("id"))
 
 	// Fetch one list.
-	single := false
 	if listID > 0 {
-		single = true
-	}
-
-	if single {
 		out, err := app.core.GetList(listID, "")
 		if err != nil {
 			return err
@@ -39,7 +30,7 @@ func handleGetLists(c echo.Context) error {
 	}
 
 	// Minimal query simply returns the list of all lists without JOIN subscriber counts. This is fast.
-	if !single && minimal {
+	if minimal {
 		res, err := app.core.GetLists("")
 		if err != nil {
 			return err
@@ -49,10 +40,12 @@ func handleGetLists(c echo.Context) error {
 		}
 
 		// Meta.
-		out.Results = res
-		out.Total = len(res)
-		out.Page = 1
-		out.PerPage = out.Total
+		out := models.PageResults{
+			Results: res,
+			Total:   len(res),
+			Page:    1,
+			PerPage: len(res),
+		}
 
 		return c.JSON(http.StatusOK, okResp{out})
 	}
@@ -63,30 +56,21 @@ func handleGetLists(c echo.Context) error {
 		return err
 	}
 
-	if single && len(res) == 0 {
-		return echo.NewHTTPError(http.StatusBadRequest,
-			app.i18n.Ts("globals.messages.notFound", "name", "{globals.terms.list}"))
+	out := models.PageResults{
+		Query:   query,
+		Results: res,
+		Total:   total,
+		Page:    pg.Page,
+		PerPage: pg.PerPage,
 	}
-
-	if single {
-		return c.JSON(http.StatusOK, okResp{res[0]})
-	}
-
-	out.Query = query
-	out.Results = res
-	out.Total = total
-	out.Page = pg.Page
-	out.PerPage = pg.PerPage
 
 	return c.JSON(http.StatusOK, okResp{out})
 }
 
 // handleCreateList handles list creation.
 func handleCreateList(c echo.Context) error {
-	var (
-		app = c.Get("app").(*App)
-		l   = models.List{}
-	)
+	app := c.Get("app").(*App)
+	var l models.List
 
 	if err := c.Bind(&l); err != nil {
 		return err
@@ -107,10 +91,8 @@ func handleCreateList(c echo.Context) error {
 
 // handleUpdateList handles list modification.
 func handleUpdateList(c echo.Context) error {
-	var (
-		app   = c.Get("app").(*App)
-		id, _ = strconv.Atoi(c.Param("id"))
-	)
+	app := c.Get("app").(*App)
+	id, _ := strconv.Atoi(c.Param("id"))
 
 	if id < 1 {
 		return echo.NewHTTPError(http.StatusBadRequest, app.i18n.T("globals.messages.invalidID"))
@@ -137,11 +119,9 @@ func handleUpdateList(c echo.Context) error {
 
 // handleDeleteLists handles list deletion, either a single one (ID in the URI), or a list.
 func handleDeleteLists(c echo.Context) error {
-	var (
-		app   = c.Get("app").(*App)
-		id, _ = strconv.ParseInt(c.Param("id"), 10, 64)
-		ids   []int
-	)
+	app := c.Get("app").(*App)
+	id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	ids := []int{}
 
 	if id < 1 && len(ids) == 0 {
 		return echo.NewHTTPError(http.StatusBadRequest, app.i18n.T("globals.messages.invalidID"))
