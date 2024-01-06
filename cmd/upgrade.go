fa2c840ddbc5c8337fb87c6938b815773d7c7379
Kailash Nadh <kailash@nadh.in>
2024-01-06 11:56:08 +0530

    Rename v2.6.0 migration to v3.0.0, the next major release.

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -36,7 +36,7 @@ var migList = []migFunc{
 	{"v2.3.0", migrations.V2_3_0},
 	{"v2.4.0", migrations.V2_4_0},
 	{"v2.5.0", migrations.V2_5_0},
-	{"v2.6.0", migrations.V2_6_0},
+	{"v3.0.0", migrations.V3_0_0},
 }
 
 // upgrade upgrades the database to the current version by running SQL migration files
--- internal/migrations/v2.6.0.go
+++ internal/migrations/v2.6.0.go
@@ -6,8 +6,8 @@ import (
 	"github.com/knadh/stuffbin"
 )
 
-// V2_6_0 performs the DB migrations.
-func V2_6_0(db *sqlx.DB, fs stuffbin.FileSystem, ko *koanf.Koanf) error {
+// V3_0_0 performs the DB migrations.
+func V3_0_0(db *sqlx.DB, fs stuffbin.FileSystem, ko *koanf.Koanf) error {
 	// Insert new preference settings.
 	if _, err := db.Exec(`INSERT INTO settings (key, value) VALUES ('bounce.postmark', '{"enabled": false, "username": "", "password": ""}') ON CONFLICT DO NOTHING;`); err != nil {
 		return err
