29ad6da1d2e96be5c4baa16535cf8297bc3b2d22
Kailash Nadh <kailash@nadh.in>
2024-05-30 23:38:14 +0530

    Make user avatar field nullable.

--- models/models.go
+++ models/models.go
@@ -158,7 +158,7 @@ type User struct {
 	Name          string              `db:"name" json:"name"`
 	Type          string              `db:"type" json:"type"`
 	Status        string              `db:"status" json:"status"`
-	Avatar        string              `db:"-" json:"avatar"`
+	Avatar        null.String         `db:"-" json:"avatar"`
 	Permissions   map[string]struct{} `db:"-" json:"-"`
 	LoggedInAt    null.Time           `db:"loggedin_at" json:"loggedin_at"`
 
