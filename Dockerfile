34d6044801d44593e8958ec06b4ca9bda4b692c4
Luis Mendes <luis.mendes@mercedes-benz.io>
2024-06-10 17:07:36 +0100

    Enabling the usage of non root user in Docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,35 @@
 FROM alpine:latest
-RUN apk --no-cache add ca-certificates tzdata
+
+# Set the maintainer information
+LABEL org.opencontainers.image.title="listmonk"
+LABEL org.opencontainers.image.description="listmonk is a standalone, self-hosted, newsletter and mailing list manager. It is fast, feature-rich, and packed into a single binary."
+LABEL org.opencontainers.image.url="https://listmonk.app"
+LABEL org.opencontainers.image.documentation='https://listmonk.app'
+LABEL org.opencontainers.image.source='https://github.com/knadh/listmonk'
+LABEL org.opencontainers.image.licenses='AGPL-3.0'
+
+# Install dependencies
+RUN apk --no-cache add ca-certificates tzdata shadow su-exec
+
+# Set the working directory
 WORKDIR /listmonk
+
+# Copy only the necessary files
 COPY listmonk .
 COPY config.toml.sample config.toml
 COPY config-demo.toml .
-CMD ["./listmonk"]
+
+# Copy the entrypoint script
+COPY docker-entrypoint.sh /usr/local/bin/
+
+# Make the entrypoint script executable
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
+
+# Expose the application port
 EXPOSE 9000
+
+# Set the entrypoint
+ENTRYPOINT ["docker-entrypoint.sh"]
+
+# Define the command to run the application
+CMD ["./listmonk"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+export UID=${UID:-0}
+export GID=${GID:-0}
+export GROUP_NAME="app"
+export USER_NAME="app"
+
+# This function evaluates if the supplied GID is already in use
+# if it is not in use, it creates the group with the GID
+# if it is in use, it sets the GROUP_NAME to the existing group
+create_group() {
+  if ! getent group ${GID} > /dev/null 2>&1; then
+    addgroup -g ${GID} ${GROUP_NAME}
+  else
+    existing_group=$(getent group ${GID} | cut -d: -f1)
+    export GROUP_NAME=${existing_group}
+  fi
+}
+
+# This function evaluates if the supplied UID is already in use
+# if it is not in use, it creates the user with the UID and GID
+create_user() {
+  if ! getent passwd ${UID} > /dev/null 2>&1; then
+    adduser -u ${UID} -G ${GROUP_NAME} -s /bin/sh -D ${USER_NAME}
+  else
+    existing_user=$(getent passwd ${UID} | cut -d: -f1)
+    export USER_NAME=${existing_group}
+  fi
+}
+
+# Run the needed functions to create the user and group
+create_group
+create_user
+
+# Set the ownership of the app directory to the app user
+chown -R ${UID}:${GID} /listmonk
+
+echo "Launching listmonk with user=[${USER_NAME}] group=[${GROUP_NAME}] uid=[${UID}] gid=[${GID}]"
+
+# If running as root and UID is not 0, then execute command as UID
+# this allows us to run the container as a non-root user
+if [ "$(id -u)" = "0" ] && [ "${UID}" != "0" ]; then
+  su-exec ${UID}:${GID} "$@"
+else
+  exec "$@"
+fi
