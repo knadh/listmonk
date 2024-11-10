c55765b55e147ce062acefd5ac0bcce4c0beab1e
Kailash Nadh <kailash@nadh.in>
2024-11-10 17:02:21 +0530

    Add DB migration for ForwardEmail bounce settings.

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -39,6 +39,7 @@ var migList = []migFunc{
 	{"v2.5.0", migrations.V2_5_0},
 	{"v3.0.0", migrations.V3_0_0},
 	{"v4.0.0", migrations.V4_0_0},
+	{"v4.1.0", migrations.V4_1_0},
 }
 
 // upgrade upgrades the database to the current version by running SQL migration files
