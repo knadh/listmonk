87803ce7505f83565d978f17d736f1c003a191a6
Kunall Banerjee <hey@kimchiii.space>
2025-02-03 07:56:46 -0500

    fix: typo in callout under the settings page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -87,7 +87,7 @@
             <b-icon icon="warning-empty" />
             Remove the <code>admin_username</code> and <code>admin_password</code> fields from the TOML
             configuration file or environment variables. If you are using APIs, create and use new API credentials
-            before removing the them. Visit
+            before removing them. Visit
             <router-link :to="{ name: 'users' }">
               Admin -> Settings -> Users
             </router-link> dashboard. <a href="https://listmonk.app/docs/upgrade/#upgrading-to-v4xx" target="_blank"
