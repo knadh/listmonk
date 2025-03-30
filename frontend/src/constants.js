b18c7ad0fbe09c6568e267e7d8e13ba1ced1861f
Kailash Nadh <kailash@nadh.in>
2025-03-30 10:53:41 +0530

    Fix incorrect loading spinner on the Lists UI. Closes #1822.

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,7 +2,12 @@ export const models = Object.freeze({
   serverConfig: 'serverConfig',
   lang: 'lang',
   dashboard: 'dashboard',
+  // This loading state is used across all contexts where lists are loaded
+  // via the instant "minimal" API.
   lists: 'lists',
+  // This is used only on the lists page where lists are loaded with full
+  // context (subscriber counts), which can be slow and expensive.
+  listsFull: 'listsFull',
   subscribers: 'subscribers',
   campaigns: 'campaigns',
   templates: 'templates',
