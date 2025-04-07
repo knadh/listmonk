fb6fd26cd7ab5b673ed5221fe53b87f04e05850a
Kailash Nadh <kailash@nadh.in>
2025-04-07 17:57:04 +0530

    Fix incorrect template states in DB in campaign creation and broken preview.

--- frontend/src/views/Templates.vue
+++ frontend/src/views/Templates.vue
@@ -34,7 +34,8 @@
         <b-tag v-if="props.row.type === 'campaign'" :class="props.row.type" :data-cy="`type-${props.row.type}`">
           {{ $tc('templates.typeCampaignHTML') }}
         </b-tag>
-        <b-tag v-else-if="props.row.type === 'campaign_visual'" :class="props.row.type" :data-cy="`type-${props.row.type}`">
+        <b-tag v-else-if="props.row.type === 'campaign_visual'" :class="props.row.type"
+          :data-cy="`type-${props.row.type}`">
           {{ $tc('templates.typeCampaignVisual') }}
         </b-tag>
         <b-tag v-else :class="props.row.type" :data-cy="`type-${props.row.type}`">
