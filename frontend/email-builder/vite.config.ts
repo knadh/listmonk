dbb2984a1df4c92eeca4d5bc3e98920570daa2dd
Kailash Nadh <kailash@nadh.in>
2025-04-18 16:48:43 +0530

    Prepare first RC.

--- frontend/email-builder/vite.config.ts
+++ frontend/email-builder/vite.config.ts
@@ -5,12 +5,13 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.NODE_ENV': '"production"'
+    'process.env.NODE_ENV': '"production"',
   },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.tsx'),
       name: 'EmailBuilder',
+      formats: ['umd'],
       fileName: (format) => `email-builder.${format}.js`,
     },
     minify: 'terser',
@@ -27,5 +28,5 @@ export default defineConfig({
     //     },
     //   },
     // }
-  }
+  },
 });
