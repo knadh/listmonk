b1febb271d3882183d1a7c2d4698348637ec29e2
Kailash Nadh <kailash@nadh.in>
2025-07-20 13:10:37 +0530

    Refactor OIDC DB migration SQL.
    - Simplify and make SQL readable.
    - Make the field addition truly idempotent per field.
    - Add missing migration in default schema.

--- internal/migrations/v5.1.0.go
+++ internal/migrations/v5.1.0.go
@@ -8,24 +8,17 @@ import (
 	"github.com/knadh/stuffbin"
 )
 
-// V5_1_0 performs the DB migrations.
 func V5_1_0(db *sqlx.DB, fs stuffbin.FileSystem, ko *koanf.Koanf, lo *log.Logger) error {
-	// Update OIDC settings to include auto_create_users and default_user_role_id fields
-	if _, err := db.Exec(`
-		UPDATE settings 
-		SET value = jsonb_set(
-			jsonb_set(
-				value::jsonb,
-				'{auto_create_users}',
-				'false'::jsonb
-			),
-			'{default_user_role_id}',
-			'0'::jsonb
-		)
-		WHERE key = 'security.oidc' AND NOT value::jsonb ? 'auto_create_users';
-	`); err != nil {
+	// Update OIDC settings to include auto_create_users and default_user_role_id fields if not present
+	_, err := db.Exec(`
+		UPDATE settings
+		SET value = value::JSONB
+			|| CASE WHEN NOT (value::JSONB ? 'auto_create_users') THEN '{"auto_create_users": false}'::JSONB ELSE '{}'::JSONB END
+			|| CASE WHEN NOT (value::JSONB ? 'default_user_role_id') THEN '{"default_user_role_id": 0}'::JSONB ELSE '{}'::JSONB END
+		WHERE key = 'security.oidc';
+	`)
+	if err != nil {
 		return err
 	}
-
 	return nil
 }
