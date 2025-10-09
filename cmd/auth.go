a5f42adc2ced78e5672f7c49066acad88aa44404
dhivyasri18 <dhivyasanthosh0111@gmail.com>
2025-10-10 01:06:01 +0200

    Updated README and fixed issue

--- cmd/auth.go
+++ cmd/auth.go
@@ -30,8 +30,8 @@ type loginTpl struct {
 }
 
 type oidcState struct {
-	Nonce string `json:"nonce"`
-	Next  string `json:"next"`
+	Nonce string json:"nonce"
+	Next  string json:"next"
 }
 
 var oidcProviders = map[string]struct{}{
@@ -345,6 +345,8 @@ func (a *App) doLogin(c echo.Context) error {
 	// Log the user in by fetching and verifying credentials from the DB.
 	user, err := a.core.LoginUser(username, password)
 	if err != nil {
+		// Log the failed login attempt
+		a.log.Printf("failed login attempt for user: %s, error: %v", username, err)
 		return err
 	}
 
