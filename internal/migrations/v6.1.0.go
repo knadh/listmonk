38553a79622e54834702e7ea8fbbb791d332dcae
bogay <pojay11523@gmail.com>
2026-01-14 00:35:25 +0800

    feat: add migration for S3 upload URI settings

---
+++ internal/migrations/v6.1.0.go
@@ -0,0 +1,21 @@
+package migrations
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/knadh/koanf/v2"
+	"github.com/knadh/stuffbin"
+)
+
+// V6_1_0 performs the DB migrations.
+func V6_1_0(db *sqlx.DB, fs stuffbin.FileSystem, ko *koanf.Koanf, lo *log.Logger) error {
+	// Insert new S3 upload URI setting.
+	if _, err := db.Exec(`
+		INSERT INTO settings (key, value) VALUES('upload.s3.upload_uri', '""') ON CONFLICT DO NOTHING;
+	`); err != nil {
+		return err
+	}
+
+	return nil
+}
