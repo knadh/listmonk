3a46967aba808e900d4895a17fe7d564e5c7f2eb
Sandy10247 <sandilyakaravadi@gmail.com>
2026-01-25 19:18:19 +0530

    fix for Create Campaign discard issue

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -249,4 +249,31 @@ export default class Utils {
     p[key] = val;
     localStorage.setItem(prefKey, JSON.stringify(p));
   };
+
+  // Source - https://stackoverflow.com/a
+  // Posted by Ebrahim Byagowi, modified by community. See post 'Timeline' for change history
+  // Retrieved 2026-01-25, License - CC BY-SA 3.0
+
+  isEqual(x, y) {
+    if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+    // after this just checking type of one would be enough
+    if (x.constructor !== y.constructor) { return false; }
+    // if they are functions, they should exactly refer to same one (because of closures)
+    if (x instanceof Function) { return x === y; }
+    // if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
+    if (x instanceof RegExp) { return x === y; }
+    if (x === y || x.valueOf() === y.valueOf()) { return true; }
+    if (Array.isArray(x) && x.length !== y.length) { return false; }
+
+    // if they are dates, they must had equal valueOf
+    if (x instanceof Date) { return false; }
+
+    // if they are strictly equal, they both need to be object at least
+    if (!(x instanceof Object)) { return false; }
+    if (!(y instanceof Object)) { return false; }
+
+    // recursive object equality check
+    const p = Object.keys(x);
+    return Object.keys(y).every((i) => p.indexOf(i) !== -1) && p.every((i) => this.isEqual(x[i], y[i]));
+  }
 }
